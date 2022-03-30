7ca485fd08b994dfd5aaa6b54d9af41967678f60
Hüseyin Şen <47068465+huseyinsenkaya@users.noreply.github.com>
2022-03-30 23:31:05 +0300

    fix responsive

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let rating = document.querySelector(".rating");
 let thanks = document.querySelector(".thanks");
 let spans = [...document.querySelectorAll(".rate-action")[0].children];
+let errorMessage = document.querySelector(".error");
+
 let button = document.querySelector(".button");
 let rateValue = 0;
 
@@ -15,14 +17,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function buttonClick() {
   let infoSelect = document.querySelector(".info-select");
-  infoSelect.textContent = `You selected ${rateValue} out of 5`;
-  rating.style.display = "none";
-  thanks.style.display = "flex";
+  let hasSelect = true;
+  spans.forEach((span) => {
+    if (span.classList.contains("selected")) {
+      hasSelect = false;
+    }
+  });
+
+  if (!hasSelect) {
+    infoSelect.textContent = `You selected ${rateValue} out of 5`;
+    rating.style.display = "none";
+    thanks.style.display = "flex";
+  } else {
+    errorMessage.style.visibility = "visible";
+  }
 }
 
 function setRating(e) {
   let currentSpan = e.target;
   currentSpan.classList.toggle("selected");
+  errorMessage.style.visibility = "hidden";
   spans.forEach((span) => {
     if (span !== currentSpan) {
       span.classList.remove("selected");
